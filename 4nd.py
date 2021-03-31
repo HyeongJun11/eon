56dc56b867c3efdebfe1e19a292b66b0a54960d6
HyeongJun11 <80689212+HyeongJun11@users.noreply.github.com>
2021-03-31 21:09:40 +0900

    Update 4nd.py

--- 4nd.py
+++ 4nd.py
@@ -2,7 +2,7 @@ x = list(map(int,input("숫자: ").split()))
 
 def bubblesort(x):
     n= len(x)
-    for i in range(0,n) :
+    for i in range(0,n):
         for j in range(n-1,i,-1):
             if x[j-1] > x[j]:
                x[j-1], x[j] = x[j], x[j-1]
